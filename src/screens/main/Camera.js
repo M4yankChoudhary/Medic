eeda06dcc18ec8204ba58bc6f47d9cb463e43d45
Mayank Choudhary <7mayankchoudhary7@gmail.com>
2021-12-11 14:43:25 +0530

    add text recognition

--- src/screens/main/Camera.js
+++ src/screens/main/Camera.js
@@ -1,4 +1,4 @@
-import React, {useState,useRef} from 'react';
+import React, {useState, useRef} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -11,37 +11,42 @@ import {
   View,
   Image,
 } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import {RNCamera} from 'react-native-camera';
+import TextRecognition from 'react-native-text-recognition';
 
-const Camera = (props) => {
-
-  let cameraRef = useRef(null)
-  const [camType,setcamType] = useState(RNCamera.Constants.Type.back);
-  const [flash,setflash] = useState(RNCamera.Constants.FlashMode.on);
+const Camera = props => {
+  let cameraRef = useRef(null);
+  const [camType, setcamType] = useState(RNCamera.Constants.Type.back);
+  const [flash, setflash] = useState(RNCamera.Constants.FlashMode.on);
+  const [ocrElements, setOcr] = useState();
   const takePicture = async () => {
     if (cameraRef) {
-      const options = { quality: 0.5, base64: true };
+      const options = {
+        quality: 0.5,
+        base64: true,
+      };
       const data = await cameraRef.current.takePictureAsync(options);
       console.log(data.uri);
+      const result = await TextRecognition.recognize(data.uri);
+      console.log(result);
       // props.navigation.navigate("Home",{uri:data.uri})
     }
   };
   const flipCamera = async () => {
     if (camType === RNCamera.Constants.Type.back) {
       setcamType(RNCamera.Constants.Type.front);
-    }
-    else{
+    } else {
       setcamType(RNCamera.Constants.Type.back);
     }
   };
   const toggleFlash = async () => {
     if (flash === RNCamera.Constants.FlashMode.on) {
       setflash(RNCamera.Constants.FlashMode.off);
-    }
-    else{
+    } else {
       setflash(RNCamera.Constants.FlashMode.on);
     }
   };
+
   return (
     <View style={styles.container}>
       <RNCamera
@@ -49,29 +54,32 @@ const Camera = (props) => {
         style={styles.preview}
         type={camType}
         flashMode={flash}
+        captureAudio={false}
         androidCameraPermissionOptions={{
           title: 'Permission to use camera',
           message: 'We need your permission to use your camera',
           buttonPositive: 'Ok',
           buttonNegative: 'Cancel',
-        }}
-      >
-      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-        <TouchableOpacity onPress={()=>toggleFlash()} style={styles.capture}>
-          <Text style={{ fontSize: 14 }}> Flash </Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={()=>takePicture()} style={styles.capture}>
-          <Text style={{ fontSize: 14 }}> SNAP </Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={()=>flipCamera()} style={styles.capture}>
-          <Text style={{ fontSize: 14 }}> TURN </Text>
-        </TouchableOpacity>
-      </View>
+        }}>
+        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+          <TouchableOpacity
+            onPress={() => toggleFlash()}
+            style={styles.capture}>
+            <Text style={{fontSize: 14}}> Flash </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => takePicture()}
+            style={styles.capture}>
+            <Text style={{fontSize: 14}}> SNAP </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => flipCamera()} style={styles.capture}>
+            <Text style={{fontSize: 14}}> TURN </Text>
+          </TouchableOpacity>
+        </View>
       </RNCamera>
     </View>
   );
-
-}
+};
 
 const styles = StyleSheet.create({
   container: {
